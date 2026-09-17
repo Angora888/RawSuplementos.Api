ab3cf743c285276492140e3f27b1888ac5c5da83
Angora888 <angora88@gmail.com>
2026-09-17 10:55:20 -0600

    Associate users with tenant

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace RawSuplementos.Api.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        public int NegocioId { get; set; }
+        public Negocio Negocio { get; set; } = null!;
 
         [Required]
         [MaxLength(120)]
