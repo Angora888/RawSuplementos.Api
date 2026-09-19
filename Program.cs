36c2ef912b4861051c53e24288f56247eb14a14b
Angora888 <angora88@gmail.com>
2026-09-19 11:27:19 -0600

    Register sales query service

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<UsuarioTenantService>();
 builder.Services.AddScoped<VentaService>();
+builder.Services.AddScoped<VentaConsultaService>();
 
 
 // =========================
